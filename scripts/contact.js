ad656add41738798f2ccea055fbc7996aeeb4dee
Rodrigo Rangel <130096450+Rodrigo1Rangel@users.noreply.github.com>
2024-04-18 18:07:33 -0600

    Fix Sumission Button Interaction

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,12 +1,12 @@
 // when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
 
-const afterSubmission = document.getElementById("submit-button")
-afterSubmission.addEventListener("click", submit)
+const submitButtonPress = document.getElementById("submit-button")
+submitButtonPress.addEventListener("click", submit)
 
 function submit() {
+    let afterSubmission = document.getElementById("contact-page")
     afterSubmission.innerHTML = '<p>Thank you for your message</p>';
     afterSubmission.classList.add("large-text");
 }
 
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
-
